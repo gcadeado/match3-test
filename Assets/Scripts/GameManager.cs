fd96c0dac68e33cffef5537d63c269aece83c6ba
Gabriel Cencic <gabriel.cencic@gmail.com>
2020-08-06 03:26:11 -0300

    Add game UI and timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,9 +9,28 @@ public class GameManager : MonoBehaviour
 
     public float levelStartDelay = 2f;  //Time to wait before starting a new level, in seconds.
 
+    public float roundTime = 120f; // Round time, in seconds
+
     private BoardManager boardScript;  // Store a reference to our BoardManager which will set up the level.
     private int level = 1;  //  Current level number.
-    private bool doingSetup = true; // Boolean to check if we're setting and updating up board, prevent various actions during setup.
+
+    private bool paused;
+
+    private float timeLeft = 0;
+
+    private int targetScore = 0;
+    private int levelScore = 0;
+
+    [SerializeField]
+    private TMP_Text levelText;	//Text to display current level number.
+    [SerializeField]
+    private TMP_Text levelTargetText; //Text to display current level target score.
+    [SerializeField]
+    private TMP_Text levelScoreText;	//Text to display current score.
+    [SerializeField]
+    private RectTransform levelScoreBar; // Bar to track level score
+    [SerializeField]
+    private TMP_Text levelTimerText;	//Text to display current score.
 
     //Awake is always called before any Start functions
     void Awake()
@@ -51,30 +70,85 @@ public class GameManager : MonoBehaviour
     static private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
     {
         instance.level++;
+
         instance.InitGame();
     }
 
     //Initializes the game for each level.
     void InitGame()
     {
-        //While doingSetup is true the player can't move, prevent player from moving while title card is up.
-        doingSetup = true;
+        // Show current round
+        StartCoroutine(ShowLevelText("Round " + level, 2.0f));
+
+        // Reset score
+        levelScore = 0;
+
+        // Set level score
+        levelScoreText.text = levelScore.ToString();
+
+        // Set level score bar to 0
+        levelScoreBar.localScale = new Vector3(0f, 1f, 1f);
+
+        // Set level target score using a simple formula
+        targetScore = (int)(level * 1000f);
+
+        //Set the text of levelTarget
+        levelTargetText.text = targetScore.ToString();
 
         //Call the Init function of the BoardManager script.
         boardScript.Init();
 
+        // Reset timer
+        timeLeft = roundTime;
+
+        //Reset the text of levelTimer
+        levelTimerText.text = timeLeft.ToString();
+
+    }
+
+    IEnumerator ShowLevelText(string text, float duration, bool pause = true)
+    {
+        if (pause)
+        {
+            paused = true;
+        }
+        //Set the text of levelText to the string "Level" and append the current level number.
+        levelText.text = text;
+
+        // TODO create a nice animation to text level
+        levelText.enabled = true;
+
+        yield return new WaitForSeconds(duration);
+
+        levelText.enabled = false;
+
+        paused = false;
     }
 
     //Update is called every frame.
     void Update()
     {
-        if (doingSetup)
+        if (paused)
+        {
             return;
+        }
+
+        // Update time left
+        timeLeft -= Time.deltaTime;
+        levelTimerText.text = ((int)timeLeft).ToString();
+
+        if (timeLeft < 0)
+        {
+            GameOver();
+        }
     }
 
     //GameOver is called when the timer reaches 0
     public void GameOver()
     {
+        // Show game over
+        StartCoroutine(ShowLevelText("Time's up!\nGame Over", 5.0f));
+
         //Disable this GameManager.
         enabled = false;
     }
